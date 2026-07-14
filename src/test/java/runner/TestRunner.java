df22335e465b77610569dcfb38e91660e8528cb3
Mohak Bhatia <161229503+Mohak-Bhatia@users.noreply.github.com>
2026-07-15 01:30:38 +0530

    Fix: Update feature file path to correct location with 'feature' directory

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Filter.feature",
+		features = "src/test/resources/feature/Filter.feature",
 		glue = {"stepDefinitions"},
 		tags = "@FilterFeature"
 		)
